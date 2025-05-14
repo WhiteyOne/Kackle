87298fb6d8c141a8d4f40c2950bd44693df5c606
Slingshot98 <coreycalhoonc@gmail.com>
2025-05-13 22:56:51 -0500

    get server by server ID with authentication login required

--- kackle/backend/app/routes/server.py
+++ kackle/backend/app/routes/server.py
@@ -1,11 +1,23 @@
 from flask import Blueprint, jsonify
-
-from app.models.db import server
+from flask_login import login_required
+from app.models.db import Server
 
 bp = Blueprint('server', __name__)
 
 ##Route Type: GET
 @bp.route('/server', methods=['GET'])
+@login_required
 def get_servers():
-    servers = server.query.all()
+    servers = Server.query.filter_by(owner_id=current_user.id).all()
     return jsonify([server.to_dict() for server in servers])
+
+##Route Type: Get 
+@bp.route(f"/server/{server_id}", methods=["GET"])
+@login_required
+def get_server(server_id):
+    server=Server.query(server_id)
+
+    ##auth check
+    if server.user_id !=current_user.id:
+        return jsonify({"error": "You do not have the right laughter level to acces this!"})
+    return jsonify(server.to_dict())
