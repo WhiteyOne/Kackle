ae9b2e3aaeae4f1e5a96ac68a871a5f8effd5b2c
WhiteyOne <Cayden.Hanks.Pro@gmail.com>
2025-05-22 17:13:14 -0700

    added folder to redux for channels

---
+++ frontend/src/redux/channels.js
@@ -0,0 +1,17 @@
+const GET_CHANNELS_BY_ID = "GET_CHANNELS_BY_ID"
+const UPDATE_CHANNELS_BY_ID = "UPDATE_CHANNELS_BY_ID"
+const DELETE_CHANNELS_BY_ID = "DELETE_CHANNELS_BY_ID"
+const CREATE_CHANNELS_BY_ID = "CREATE_CHANNELS_BY_ID"
+
+
+
+
+
+
+const getChannelByIdThunk=(id,userId)=> async (dispatch) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
