824f92b5ef959f4d66d2e0ca01c0ca32cd246038
WhiteyOne <Cayden.hanks.pro@gmail.com>
2025-05-30 16:14:13 -0700

    Update messages_routes.py

--- backend/app/api/messages_routes.py
+++ backend/app/api/messages_routes.py
@@ -7,19 +7,19 @@
 # messages_routes = Blueprint("messages", __name__)
 
 
-<<<<<<< HEAD
-@messages_routes.route("/<int:server_id>/channel/<int:channel_id>/messages", methods=["GET"])
-@login_required
-def get_channel_messages(channel_id):
-    messages = Channel_Message.query.filter_by(channel_id=channel_id).all()
-    return jsonify([message.to_dict() for message in messages])
-=======
+# <<<<<<< HEAD
 # @messages_routes.route("/<int:server_id>/channel/<int:channel_id>/messages", methods=["GET"])
 # @login_required
-# def get_channel_messages(server_id, channel_id):
+# def get_channel_messages(channel_id):
 #     messages = Channel_Message.query.filter_by(channel_id=channel_id).all()
 #     return jsonify([message.to_dict() for message in messages])
->>>>>>> 2bba975c880c979e4c31ae5a1652fd3dd72c5332
+# =======
+# # @messages_routes.route("/<int:server_id>/channel/<int:channel_id>/messages", methods=["GET"])
+# # @login_required
+# # def get_channel_messages(server_id, channel_id):
+# #     messages = Channel_Message.query.filter_by(channel_id=channel_id).all()
+# #     return jsonify([message.to_dict() for message in messages])
+# >>>>>>> 2bba975c880c979e4c31ae5a1652fd3dd72c5332
 
 
 # # ---- Create Message ---- 
